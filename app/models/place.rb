8cd0b3159447ca7095dfdf519514f0fec26cbdbb
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-07-11 00:50:00 +0000

    Added an API key

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode 
+  
+  validates :name, presence: true, length: { minimum: 3, maximum: 60 }
+  validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 4 }
 end
