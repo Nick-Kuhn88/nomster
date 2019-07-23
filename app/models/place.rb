ab4e15a6d67c563f0f99687dd1fa21528a004b73
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-07-23 20:45:15 +0000

    Fixed up the code that should show the picture and caption

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode 
   
-  validates :name, presence: true, length: { minimum: 3, maximum: 60 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 80 }
   validates :address, presence: true
   validates :description, presence: true, length: { minimum: 4 }
 end
