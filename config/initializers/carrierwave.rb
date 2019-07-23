07cb5f64f3fcc4be795a5b395df9d42431958b32
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-07-23 22:52:16 +0000

    Commiting befor a migrate

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,25 @@
 # config/initializers/carrierwave.rb
-require 'carrierwave/storage/fog'
+#require 'carrierwave/storage/fog'
+# CarrierWave.configure do |config|
+#   config.fog_provider = 'fog/aws'                        # required
+#   config.fog_public = false
+#   config.fog_credentials = {
+#     provider:              'AWS',                        # required
+#     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+#     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+#   }
+#   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+#   config.storage = :fog 
+# end
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  config.fog_provider = 'fog/aws'
   config.fog_public = false
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    provider:               'AWS',
+    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
+    aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
   }
-  config.storage = :fog
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
+  config.fog_directory = ENV["AWS_BUCKET"]
 end
