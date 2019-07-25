bcf43ca4d687cbe2fa52e9082bf20f41e24ea694
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-07-25 21:21:37 +0000

    Created a new AWS bucket and user and now carrierwave WORKS and image uploading WORKS and it's GREATgit add --all!

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,24 @@
 # config/initializers/carrierwave.rb
-require 'carrierwave/storage/fog'
-# CarrierWave.configure do |config|
-#   config.fog_provider = 'fog/aws'                        # required
-#   config.fog_public = false
-#   config.fog_credentials = {
-#     provider:              'AWS',                        # required
-#     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-#     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-#   }
-#   config.fog_directory  = ENV["AWS_BUCKET"]              # required
-#   config.storage = :fog 
-# end
-
+#require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_public = false
   config.fog_credentials = {
-    provider:               'AWS',
-    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
-    aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-
-  config.fog_directory = ENV["AWS_BUCKET"]
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
+
+# CarrierWave.configure do |config|
+#   config.fog_provider = 'fog/aws'
+#   config.fog_public = false
+#   config.fog_credentials = {
+#     provider:               'AWS',
+#     aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
+#     aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
+#   }
+
+#   config.fog_directory = ENV["AWS_BUCKET"]
+#end
